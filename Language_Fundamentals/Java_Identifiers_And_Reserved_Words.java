fc02796c6241a1d7e54bfbf04cc8de0c5e9a4a34
Pankaj Singh Negi <dsashayarpankaj@gmail.com>
2025-09-03 20:18:14 +0530

    Code_Updated

---
+++ Language_Fundamentals/Java_Identifiers_And_Reserved_Words.java
@@ -0,0 +1,37 @@
+//Identifiers and Reserved Words in Java
+
+//-----------------------------------------------------------------------------------<<<<1.Identifiers>>>>---------------------------------------------------------------------------------------
+
+//Identifier:-A name in Java Program is called Identifier which can be used for identification purpose.It can be used for naming variables,methods,classes,packages and interfaces.
+
+/*
+Ex:-
+class Test
+{
+    public static void main(String args[])
+    {
+        int a=10; 
+        
+    }
+}
+
+Identifiers:-  5  ----->Test(class name),main(method name),args(array name),a(variable name),String(Predefinedclass name)
+
+
+//----------------------------------------------------------------<<<<2.Rules to create/or define Identifiers in Java>>>>-------------------------------------------------------------------------
+
+
+1.Java allows only the following characters for identifiers:-
+   a.Alphabets(A-Z,a-z)
+   b.Digits(0-9)
+   c.Underscore(_)
+   d.Dollar($)
+if we are using any other characters except these we will get compile time error.
+
+Ex:- total_number()  ----->valid
+     total#number()  ----->invalid
+
+
+
+
+ */
